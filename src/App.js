bdbc1456bdc238e9605ec2422a1f68930e9aebb2
Lee <leeblazek@gmail.com>
2020-05-12 11:58:20 -0600

    Step 2: basic useEffect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 
 function App() {
   // Declare a new state variable, which we'll call "count"
-const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div className="App">
       <header className="App-header">
