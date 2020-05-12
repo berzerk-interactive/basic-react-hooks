592a2e682dd2baf8a0d372b692c4e8136c0c2c9c
Lee <leeblazek@gmail.com>
2020-05-12 11:44:23 -0600

    Step 1: basic useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +12,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
       </header>
+
     </div>
   );
 }
